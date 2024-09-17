eb975db9f7f1bde5be71a4d42db11113da6cdcc1
Hamad <hamad.sa.ghosen@gmail.com>
2024-09-17 19:48:26 +0300

    Add post_list, post_details

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, decorat
+from .models import Post
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
-def home(request):
-    return render(request,'home.html')
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request,'blog/post/post_list.html', {'posts':posts})
+
+def post_details(request, id):
+#method 1
+    # try:
+    #     post = Post.objects.grt(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404('No post found')
+    # return render(request, 'post_details.html', {'post':post})
+
+#method 2
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'blog/post/post_details.html', {'post':post})
