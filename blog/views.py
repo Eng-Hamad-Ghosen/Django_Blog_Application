908f46fdadb84602fbfcfaa8dfc4ac06471ff210
Hamad <hamad.sa.ghosen@gmail.com>
2024-09-18 13:59:06 +0300

    Add paginator to the post list

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,19 @@
+import random
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.http import HttpResponse
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.all()
+    paginator = Paginator(posts,3)
+    page_number = request.GET.get('page')
+    try:
+        posts = paginator.get_page(page_number)
+    except EmptyPage:
+        posts = paginator.get_page(paginator.page_number)
     return render(request,'blog/post/post_list.html', {'posts':posts})
 
 def post_details(request, year, month, day, slug):
