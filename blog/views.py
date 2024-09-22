a5bb6c68a8d155199199543c3b1e78afef6a9d19
Hamad <hamad.sa.ghosen@gmail.com>
2024-09-22 18:39:20 +0300

    Create custom templates tags to show a list of the latest posts and most commented posts in the sidebar

--- blog/views.py
+++ blog/views.py
@@ -48,7 +48,9 @@ def post_details(request, year, month, day, slug):
                              publish__month=month,
                              publish__day=day)
     comment_count = Comment.objects.filter(post=post).count()
-    
+    print('*******')
+    print(post.post_comment.values_list('name', flat=True))
+    print('*******')
     post_tags_ids = post.tags.values_list('id', flat=True)
     similar_posts = Post.objects.filter(tags__in=post_tags_ids).exclude(id=post.id)
     similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')
